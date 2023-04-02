3d2fb0262ec89a8eb5b06d909eb170b91745c9a1
Sandipan Guha <sandi.guha@gmail.com>
2023-04-02 16:03:09 +0000

    replaced time.sleep with weddriverwait

--- modules.py
+++ modules.py
@@ -4,6 +4,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def configure_driver(url: str):
   
@@ -13,8 +15,12 @@ def configure_driver(url: str):
   chrome_options.add_argument('--disable-dev-shm-usage')
   driver = webdriver.Chrome(options=chrome_options)
   driver.get(url)
-  import time
-  time.sleep(2)  #to properly load all the contents in given url
+  #import time
+  #time.sleep(2)  #to properly load all the contents in given url
+  try:
+    WebDriverWait(driver,timeout=10).until(EC.title_is('Explore / Twitter'))
+  except webdriver.TimeoutException:
+    print("Error: URL didn't load correctly, please check your internet connection speed")
   return driver
 
 import pandas
