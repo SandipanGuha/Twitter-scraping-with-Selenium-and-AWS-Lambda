25bba4c1f41933f4dab67250cfdd3a4fb190a4bc
Sandipan Guha <>
2023-04-03 06:44:32 +0000

    completed core functionality with selenium and saving data to csv works fine; had to replace webdriverwait with time.sleep as ran into zero data issue

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 #License: MIT 
 
 import modules
+import re
 
 target_url = 'https://twitter.com/explore/tabs/trending'
 
@@ -11,6 +12,40 @@ target_url = 'https://twitter.com/explore/tabs/trending'
 if __name__ == "__main__":
   
   api_driver = modules.configure_driver(target_url)
-  get_elements = api_driver.find_elements
+  
   print(api_driver.title)
+  def trending_data(driver):
+    get_elements = api_driver.find_elements    
+    divs = get_elements(modules.By.CLASS_NAME,'r-1adg3ll')
+    trending=[]
+    append = False
+    for i in divs:
+      for j in i.find_elements(modules.By.TAG_NAME,'span'):
+        if re.match(r'^1$',j.text) or re.match(r'^11$',j.text):
+          append = not append
+        
+        if append and j.text!='·':
+          trending.append(j.text)
+    out=[]
+    temp=[]
+    for item in trending:
+    
+      if re.match(r'^([1-9]|10)$',item):
+        if temp:
+          out.append(temp.copy())
+          temp.clear()
+      else:
+        temp.append(item)
+    else:
+      out.append(temp.copy())
+      del temp
+    return out
+    
+  df = modules.pandas.DataFrame(trending_data(api_driver))
+  #df.to_csv("Top 10 Twitter trends.csv")
+  print(df)
+        
+        
+        
+  
   
--- modules.py
+++ modules.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 def configure_driver(url: str):
   
@@ -15,12 +16,12 @@ def configure_driver(url: str):
   chrome_options.add_argument('--disable-dev-shm-usage')
   driver = webdriver.Chrome(options=chrome_options)
   driver.get(url)
-  #import time
-  #time.sleep(2)  #to properly load all the contents in given url
-  try:
-    WebDriverWait(driver,timeout=10).until(EC.title_is('Explore / Twitter'))
-  except webdriver.TimeoutException:
-    print("Error: URL didn't load correctly, please check your internet connection speed")
+  import time
+  time.sleep(20)  #to properly load all the contents in given url
+  '''try:
+    WebDriverWait(driver,timeout=10).until(EC.textToBePresentInElement('span','India trends'))
+  except TimeoutException:
+    print("Error: URL didn't load correctly, please check your internet connection speed")'''
   return driver
 
 import pandas
